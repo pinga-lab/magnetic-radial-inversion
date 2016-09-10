d4ce7de80eebe5f1d80d3f1cd8a30e8ebbdffe29
leobvital <leobvital@gmail.com>
2016-09-10 18:35:19 -0300

    added a code for poly-prism with eqlayer and corrected layer depth

--- code/mag_sphere_functions.py
+++ code/mag_sphere_functions.py
@@ -455,7 +455,7 @@ def sm_btb_sphere(x, y, z, xs, ys, zs, incs, decs):
     R = (3./(4.*np.pi))**(1./3.)
    
     for j, (xf, yf, zf) in enumerate(zip(xs,ys,zs)):
-        m = [xf, yf, zf, R, 1.]
+        m = [xf, yf, zf, R]
                 
         A[:,j] = (kernelxx_sphere(x,y,z,m)*mx + \
                   kernelxy_sphere(x,y,z,m)*my + \
