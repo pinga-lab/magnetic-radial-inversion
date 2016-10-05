df4c325c773d60e2688aa654ee7f00488d37229b
Vanderlei C Oliveira Jr <vandscoelho@gmail.com>
2016-10-05 11:58:11 -0300

    pequenas correcoes na documentacao e inclusao das coordenadas da origem

--- code/mag_polyprism_functions.py
+++ code/mag_polyprism_functions.py
@@ -9,32 +9,40 @@ from fatiando.constants import CM, T2NT
 
 ### Functions for the foward problem using fatiando
 
-def pol2cart(m, Np, Nv):
+def pol2cart(m, x0, y0, Np, Nv):
     '''
-    This function transforms polar coordinates from the prisms
-    into cartesian coordinates and return.
+    This function transforms polar coordinates of the prisms
+    into Cartesian coordinates and return a list of polygonal
+    prisms of the Fatiando a Terra.
     
     input
     
-    m: [verts,z0,R,magnetization] - verts in polar coordinates
-    Np: number of prisms
-    Nv: number of vertices per prism
+    m: list - each element is a list [verts,z0,R,magnetization] 
+    containing the vertices in polar coordinates
+    Np: int - number of prisms
+    Nv: int - number of vertices per prism
     
     output
     
-    mk: list - list of classes of polygonal prisms from fatiando
+    mk: list - list of objects of the class
+    fatiando.mesher.PolygonalPrism
     '''
     
     mk = []
-    verts = []
-    vertsk = []
+    verts = []  # it contains vertices in polar coordinates
+    vertsk = [] # it contains vertices in Cartesian coordinates
  
     assert len(m) == Np, 'The size of m and the number of prisms must be equal'
-
+    
     for mv in m:
+        assert len(mv[0]) == Nv, 'All prisms must have Nv vertices'
+        
+    assert len(m) = x0.size ...
+
+    for i, mv in enumerate(m):
         verts = mv[0]
         for v in verts:
-            vertsk.append([v[0]*np.cos(v[1]), v[0]*np.sin(v[1])])
+            vertsk.append([v[0]*np.cos(v[1]) + x0[i], v[0]*np.sin(v[1]) + y0[i]])
         mk.append(PolygonalPrism(vertsk, mv[1], mv[2], mv[3]))
         
         
